2c20c0974f94f1afb4de386f54bfbac634fb6271
jjgouveia <gouvik.dev@gmail.com>
2023-07-24 20:53:59 -0300

    chore: criando relacionamentos da tabela User

--- src/main/java/com/bankapi/Santander/Dev/Week/domain/model/User.java
+++ src/main/java/com/bankapi/Santander/Dev/Week/domain/model/User.java
@@ -1,13 +1,29 @@
 package com.bankapi.Santander.Dev.Week.domain.model;
 
-import java.util.List;
+import jakarta.persistence.*;
 
+import java.util.List;
+@Entity(name="tb_user")
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(nullable = false, length = 120)
   private String name;
+
+  @OneToOne(cascade = CascadeType.ALL)
+
   private Account account;
+  @OneToOne(cascade = CascadeType.ALL)
   private Card card;
+
+  @OneToMany(cascade = CascadeType.ALL, fetch= FetchType.EAGER)
   private List<Feature> features;
+/*
+* Fetch type EAGER: carrega todos os dados de uma vez
+* */
+  @OneToMany(cascade = CascadeType.ALL, fetch= FetchType.EAGER)
   private List<News> news;
 
   public Long getId() {
