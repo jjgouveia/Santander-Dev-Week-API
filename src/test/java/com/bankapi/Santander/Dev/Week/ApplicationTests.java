12d0fbdb7513522bcda7820db79c938e7a4fe5db
jjgouveia <gouvik.dev@gmail.com>
2023-07-24 15:05:55 -0300

    build: configurações iniciais

---
+++ src/test/java/com/bankapi/Santander/Dev/Week/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankapi.Santander.Dev.Week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
