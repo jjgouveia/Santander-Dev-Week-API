d1666477b44beb9a9c0a4b7b4569bd0782ae823e
jjgouveia <gouvik.dev@gmail.com>
2023-07-24 21:04:20 -0300

    chore: mapeando Card

--- src/main/java/com/bankapi/Santander/Dev/Week/domain/model/Card.java
+++ src/main/java/com/bankapi/Santander/Dev/Week/domain/model/Card.java
@@ -1,12 +1,19 @@
 package com.bankapi.Santander.Dev.Week.domain.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity(name ="tb_card")
 public class Card {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(unique = true)
   private String number;
 
+  @Column(precision = 2, scale = 13)
   private BigDecimal limit;
 
   public Long getId() {
